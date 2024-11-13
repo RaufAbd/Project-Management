d2ef7c91b9e71eef8b10e5e5e5ecc07bfba0ac29
RaufAbd <rufabad.4@gmail.com>
2024-11-13 22:06:55 +0400

    added functionality to NewProject component

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,25 +1,64 @@
+import { useRef, useState } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject() {
+export default function NewProject({ onSaveProject, onClose }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  const modal = useRef();
+
+  function handleSavingNewProject() {
+    const titleValue = title.current.value;
+    const descriptionValue = description.current.value;
+    const dueDateValue = dueDate.current.value;
+
+    if (titleValue.trim() && descriptionValue.trim() && dueDateValue.trim()) {
+      onSaveProject({
+        title: titleValue,
+        description: descriptionValue,
+        dueDate: dueDateValue,
+      });
+    } else modal.current.openModal();
+  }
+
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input label="Title" />
-        <Input label="Description" textarea />
-        <Input label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Oops ... looks like you forgot to enter a value.
+        </p>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              onClick={onClose}
+              className="text-stone-800 hover:text-stone-950"
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSavingNewProject}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input ref={dueDate} label="Due Date" type="date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
